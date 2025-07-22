3391f212dc8873a1cd4297664dd1720962c7c3d3
a772616239 <772616239@qq.com>
2025-07-22 11:10:30 +0800

    修复活动白片问题

--- Assets/Scripts/Google/GoogleSignMgr.cs
+++ Assets/Scripts/Google/GoogleSignMgr.cs
@@ -70,7 +70,7 @@ public class GoogleSignMgr : MonoBehaviour
             string deviceToken = "gt" + id;
             if (Application.isEditor)
             {
-                deviceToken = "gt2" + id;
+                deviceToken = "gt34" + id;
             }
             GetAcc(deviceToken, out name, out pw);
 
